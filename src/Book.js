e28fcecd88866ab3fd6fee64d53d2f8a5584e099
tkubrak <Pre@36670>
2021-02-16 18:40:22 +0200

    feat: set default shelf option on main page

--- src/Book.js
+++ src/Book.js
@@ -19,7 +19,7 @@ class Book extends Component {
                         backgroundImage: `url(${!book.imageLinks ? '' : book.imageLinks.thumbnail})`
                     }}></div>
                     <div className="book-shelf-changer">
-                        <select onChange={(e) => {onBookShelfChange(book, e.target.value)}}>
+                        <select value={book.shelf || 'none'} onChange={(e) => {onBookShelfChange(book, e.target.value)}}>
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
