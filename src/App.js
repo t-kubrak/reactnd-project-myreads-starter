84c84a451aa1b4622568cd7e6388b617faecdb20
tkubrak <Pre@36670>
2021-02-16 13:35:05 +0200

    feat: get books from api and add them to app state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BooksList from './BooksList';
 import BooksSearch from './BooksSearch';
 import {Route} from 'react-router-dom';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+    state = {
+        books: []
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <Route exact path='/' render={() => (
-          <BooksList />
-        )} />
-        <Route path='/search' render={({history}) => (
-          <BooksSearch />
-        )} />
-      </div>
-    )
-  }
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then((books) => {
+                console.log(books);
+                this.setState(() => ({books}))
+            })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <Route exact path='/' render={() => (
+                    <BooksList books={this.state.books} />
+                )}/>
+                <Route path='/search' render={({history}) => (
+                    <BooksSearch/>
+                )}/>
+            </div>
+        )
+    }
 }
 
 export default BooksApp
